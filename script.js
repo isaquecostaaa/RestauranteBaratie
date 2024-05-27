5a3193515252531bfee45332ad1a9f24896ebb7f
isaquecostaaa <167151390+isaquecostaaa@users.noreply.github.com>
2024-05-27 02:19:06 +0000

    validação de campos obrigatórios

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-aaaaaaaaaaaaaaaaaaaa
+const nome = "João";
+const pedidoRefeicao = "salmão";
+const refeicaoQtde = 1;
+const pedidoBebida = "Refrigerante";
+const bebidaQtde = 1;
+const mesa = 1;
+
+if (nome && pedidoRefeicao && refeicaoQtde && pedidoBebida && bebidaQtde && mesa) {
+alert(`Nome: ${nome}, Mesa: ${mesa}, Pedido: ${refeicaoQtde}x ${pedidoRefeicao} e ${bebidaQtde}x ${pedidoBebida}`)
+} else {
+  alert("Dados incompletos");
+}
